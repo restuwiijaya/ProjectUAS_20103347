f4af7d4987e9107d8eccd37d2f31390ece53b971
restuwiijaya <95153646+restuwiijaya@users.noreply.github.com>
2022-06-30 21:01:58 +0800

    Add files via upload

---
+++ ProMHS.java
@@ -0,0 +1,13 @@
+package promhs;
+
+/**
+ *
+ * @author Restu Wijays
+ */
+public class ProMHS {
+
+    public static void main(String[] args) {
+       new frmSIM().setVisible(true);
+    }
+    
+}
